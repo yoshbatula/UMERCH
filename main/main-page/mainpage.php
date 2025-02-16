70a314941ade57948deb3ef842040212dfafb24a
yoshbatula <yoshbatula2@gmail.com>
2025-02-16 22:20:41 +0800

    i still didn't finish the navigation

--- main/main-page/mainpage.php
+++ main/main-page/mainpage.php
@@ -8,42 +8,41 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
+
 <header>
-    <!-- FOR NAVIGATION BAR -->
-        <nav class="nav-bar">
-            <div class="logo-menu">
-                <div class="logo-container">
-                    <img class="logo" src="/assests/umerch_logo.png" alt="UMERCH Logo">
-                </div>
-                    <div class="menu-container">
-                        <a href="#">HOME</a>
-                        <a href="#">SHOP</a>
-                        <a href="#">ABOUT US</a>
-                        <a href="#">CONTACT US</a>
-                    </div>
-            </div>
-            
-                <div class="search-bar">
-                    <input type="text" placeholder="Search...">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </div>
-                <div class="sidebar">
-                    <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-                    <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
-                    <a href="#"><i class="fa-solid fa-bell"></i></a>
-                    <a href="#"><i class="fa-solid fa-user"></i></a>
-                </div>
-        </nav>
-    </header>
-    <section class="welcome-container">
-        <div class="background-image">
-            <img src="" alt="">
+    <nav class="nav-bar">
+        <div class="logo-container">
+            <img class="logo" src="/assests/umerch_logo.png" alt="UMERCH Logo">
         </div>
-    </section>
+        <div class="navlist">
+            <a href="#">HOME</a>
+            <a href="#">SHOP</a>
+            <a href="#">ABOUT US</a>
+            <a href="#">CONTACT US</a>
+        </div>
+        <div class="search-bar">
+            <input type="text" placeholder="Search...">
+            <i class="fa-solid fa-magnifying-glass"></i>
+        </div>
+        <div class="right-list">
+            <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
+            <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
+            <a href="#"><i class="fa-solid fa-bell"></i></a>
+            <a href="#"><i class="fa-solid fa-user"></i></a>
+        </div>
+    </nav>
+</header>
+
+<section class="welcome-container">
+    <div class="background-image">
+        <img src="/assests/um5background.jpg" alt="">
+    </div>
+</section>
+
 </body>
 </html>
