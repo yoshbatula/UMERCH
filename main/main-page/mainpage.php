e1f24426ecaaa0aede2ad26c01d36a814c6801b3
yoshbatula <yoshbatula2@gmail.com>
2025-02-21 23:48:37 +0800

    progress for today

--- main/main-page/mainpage.php
+++ main/main-page/mainpage.php
@@ -4,50 +4,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DASHBOARD</title>
-    <link rel="stylesheet" href="/css/mainpage.css">
+    <link rel="stylesheet" href="../../css/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 </head>
 <body>
-
-<header>
-    <nav class="nav-bar">
-        <div class="logo-container">
-            <img class="logo" src="/assests/umerch_logo.png" alt="UMERCH Logo">
-        </div>
-        <div class="navlist">
-            <a href="#">HOME</a>
-            <a href="#">SHOP</a>
-            <a href="#">ABOUT US</a>
-            <a href="#">CONTACT US</a>
-        </div>
-        <div class="search-bar">
-            <input type="text" placeholder="Search...">
-            <i class="fa-solid fa-magnifying-glass"></i>
-        </div>
-        <div class="right-list">
-            <a href="#"><i class="fa-solid fa-cart-shopping"></i></a>
-            <a href="#"><i class="fa-solid fa-bag-shopping"></i></a>
-            <a href="#"><i class="fa-solid fa-bell"></i></a>
-            <a href="#"><i class="fa-solid fa-user"></i></a>
-        </div>
-    </nav>
-</header>
-
-<section class="welcome-container">
-    <div class="background-image">
-        <img src="/assests/um5background.jpg" alt="">
+<nav class="navbar navbar-expand-xl navbar-light" style="background-color: #BE0002;">
+  <div class="container-fluid">
+    <img class="" src="/assests/UMERCHLOGO.png" alt="" style="margin-right: 30px">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLight" aria-controls="navbarLight" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse show" id="navbarLight">
+      <ul class="navbar-nav me-auto mb-2 ml-4 mb-xl-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#" style="color: white; font-family: 'Montserrat';">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" style="color: white; font-size: 'Montserrat';">SHOP</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#" tabindex="-1" aria-disabled="true" style="color: white; font-family: 'Montserrat';">ABOUT US</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+      <div class="input-group">
+    <input type="text" class="form-control" placeholder="Search" aria-label="Username" aria-describedby="input-group-right">
+    <span class="input-group-text" id="input-group-right-example"><i class="fa-solid fa-magnifying-glass"></i></span>
+</div>
+</div>
+      </form>
+      <ul class="nav justify-content-end">
+    <!-- <li class="nav-item">
+        <a class="nav-link active" aria-current="page" href="#">Active</a>
+    </li> -->
+    <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa-solid fa-cart-shopping" style="color: white;"></i>  </a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa-solid fa-bag-shopping" style="color: white;"></i></a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#"><i class="fa-solid fa-bell" style="color: white"></i></a>
+    </li>
+    </ul>
     </div>
-    <div class="welcome-message">
-        <small>CASUAL & EVERYDAY</small>
-        <P>Effortlessly combine comfort with</P>
-        <p>campus style!</p>
-    </div>
-</section>
-
+  </div>
+</nav>
+  <div class="background-img">
+      <img src="/assests/background-landingpage.png" alt="">
+  </div>
 </body>
 </html>
