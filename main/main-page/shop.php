98361dcb8261b3ee44fd5fec997382372c53a941
yoshbatula <yoshbatula2@gmail.com>
2025-03-03 23:33:37 +0800

    progress

---
+++ main/main-page/shop.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SHOP</title>
+    <link rel="stylesheet" href="/css/bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <style>
+        #navbar {
+            background-color: #B02A24;
+            height: 67px;
+        }
+        #logo {
+            margin-left: 20px;
+        }
+    </style>
+    <nav class="d-flex flex-row space-x-12" id="navbar">
+        <div class="ml-5" id="logo">
+            <img src="/assests/UMERCHLOGO.png" width="163" height="60" class="navbar-brand ml-2" alt="">
+        </div>
+        <div class="ml-5" id="logo">
+            <img src="/assests/UMERCHLOGO.png" width="163" height="64" class="navbar-brand ml-2" alt="">
+        </div><div class="ml-5" id="logo">
+            <img src="/assests/UMERCHLOGO.png" width="163" height="64" class="navbar-brand ml-2" alt="">
+        </div><div class="ml-5" id="logo">
+            <img src="/assests/UMERCHLOGO.png" width="163" height="64" class="navbar-brand ml-2" alt="">
+        </div>
+    </nav>
+</body>
+</html>
