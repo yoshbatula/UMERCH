78b2c94881d65e55a7dbb7ea3882a7cf81526110
yoshbatula <yoshbatula2@gmail.com>
2025-03-04 14:44:15 +0800

    progressing for shop

--- main/main-page/shop.php
+++ main/main-page/shop.php
@@ -4,13 +4,61 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SHOP</title>
-    <link rel="stylesheet" href="/css/bootstrap/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 </head>
-<body class="vh-100 overflow-hidden">
-    <nav style="background-color: #BE0002; height: 60px;">
-        <div class="justify-content-center w-30">
-            <input type="text" class="form-control " placeholder="Search" aria-label="Username" aria-describedby="input-group-right">
+<body>
+
+<nav class="navbar navbar-expand-lg navbar-custom" style="background-color: #BE0002;">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+            <img src="/assests/UMERCHLOGO.png" alt="UMERCH Logo" height="40">
+        </a>
+
+        <form class="d-flex ms-auto">
+            <input class="form-control me-2 search-bar" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-light" type="submit">🔍</button>
+        </form>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item text-white">
+                    <a class="nav-link" href="main-page/home.php">HOME</a>
+                </li>
+                <li class="nav-item text-white">
+                    <a class="nav-link" href="main-page/shop.php">SHOP</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="main-page/about.php">ABOUT US</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="main-page/contact.php">CONTACT US</a>
+                </li>
+            </ul>
         </div>
-    </nav>
+
+        <div class="navbar-icons d-flex">
+            <a href="#"><i class="bi bi-cart"></i></a>
+            <a href="#"><i class="bi bi-bell"></i></a>
+            <a href="#"><i class="bi bi-person-circle"></i></a>
+        </div>
+    </div>
+</nav>
+<div class="d-flex">
+    <div class="label-check-form text-center">
+        <img src="/assests/SHOPBG.png" alt="">
+        <div class="position-relative b-2 border-dark">
+            <p>PRODUCT LIST</p>
+        </div>
+    </div>
+    
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.js"></script>
+
 </body>
 </html>
