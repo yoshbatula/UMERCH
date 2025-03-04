d848e33adbeddfd27ffb9b4f0e4432ea2447bb67
yoshbatula <yoshbatula2@gmail.com>
2025-03-04 11:16:15 +0800

    progress for today

--- main/main-page/shop.php
+++ main/main-page/shop.php
@@ -6,37 +6,10 @@
     <title>SHOP</title>
     <link rel="stylesheet" href="/css/bootstrap/css/bootstrap.min.css">
 </head>
-<body>
-    <nav class="navbar navbar-expand-xl navbar-light" style="background-color: #BE0002;">
-        <div class="container-fluid">
-            <img class="" src="/assests/UMERCHLOGO.png" alt="" style="margin-right: 30px">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarLight" aria-controls="navbarLight" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse show" id="navbarLight">
-                <ul class="navbar-nav me-auto mb-2 ml-4 mb-xl-0">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#" style="color: white; font-family: 'Montserrat';">HOME</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white; font-family: 'Montserrat';">SHOP</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white; font-family: 'Montserrat';">ABOUT US</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#" style="color: white; font-family: 'Montserrat';">CONTACT US</a>
-                    </li>
-                </ul>
-                <div class="d-flex justify-content-center w-20">
-                    <form action="#" class="d-flex" style="width: 300px;">
-                        <div class="input-group w-100">
-                            <input type="text" class="form-control" placeholder="Search" aria-label="Search">
-                            <span class="input-group-text"><i class="fa-solid fa-magnifying-glass"></i></span>
-                        </div>
-                    </form>
-                </div>
-            </div>
+<body class="vh-100 overflow-hidden">
+    <nav style="background-color: #BE0002; height: 60px;">
+        <div class="justify-content-center w-30">
+            <input type="text" class="form-control " placeholder="Search" aria-label="Username" aria-describedby="input-group-right">
         </div>
     </nav>
 </body>
