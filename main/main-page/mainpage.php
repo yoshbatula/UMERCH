4eb0db847226ed0b8b8480c4abfa874094c1268e
yoshbatula <yoshbatula2@gmail.com>
2025-03-06 04:02:29 +0800

    still progressing for the landing page

--- main/main-page/mainpage.php
+++ main/main-page/mainpage.php
@@ -47,7 +47,7 @@
               <div class="overlay">
                 <h3>20% Off on T-Shirts</h3>
                 <small>Get the best deals on your favorite items.</small>
-                <button class="btn btn-warning text-white" style="width: 121px; height: 40px; font-size: 12px;">SHOP NOW</button>
+                <button class="btn-featured btn text-white" style="font-size: 12px;">SHOP NOW</button>
               </div>
             </div>
           </div>
@@ -57,7 +57,7 @@
               <div class="overlay">
                 <h3>20% Off on T-Shirts</h3>
                 <small>Get the best deals on your favorite items.</small>
-                <button class="btn btn-warning text-white" style="width: 121px; height: 40px; font-size: 12px;">SHOP NOW</button>
+                <button class="btn-featured btn text-white" style="font-size: 12px;">SHOP NOW</button>
               </div>
             </div>
           </div>
@@ -67,10 +67,215 @@
               <div class="overlay">
                 <h3>20% Off on T-Shirts</h3>
                 <small>Get the best deals on your favorite items.</small>
-                <button class="btn btn-warning text-white" style="width: 121px; height: 40px; font-size: 12px;">SHOP NOW</button>
+                <button class="btn-featured btn text-white" style="font-size: 12px;">SHOP NOW</button>
               </div>
             </div>
           </div>
+        </div>
+      </div>
+    </section>
+    <section style="background-color: #F6F6F6;">
+      <div class="d-flex mt-5" style="background-color: #F6F6F6;">
+        <div class="container">
+          <div class="text-center mt-5 mb-5">
+            <h1 style="font-size: 42px;">Featured Products</h1>
+            <hr>
+          </div>
+            <div class="row">
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container mt-5 text-white">
+        <div class="limited-time-offer">
+          <img src="/assets/images/yeppeo.png" alt="">
+            <div class="overlay">
+              <h5 class="mb-4">Limited Time Offer</h5>
+              <h1 class="mb-4">Special Edition</h1>
+              <small>Lorem ipsum dolor sit amet</small>
+              <small>consecetur. Purus aenean</small>
+              <small class="mb-4">bibendum ipsum ligula sit velit nibh.</small>
+              <h5>Buy T-shirt at 20% Discount,</h5>
+              <h5 class="mb-4">Use Code OFF20</h5>
+              <button class="btn-featured btn text-white" style="font-size: 12px;">SHOP NOW</button>
+            </div>
+        </div>
+      </div>
+      <div class="d-flex mt-5">
+        <div class="container">
+          <div class="text-center mb-5 mt-5">
+            <h1 style="font-size: 42px;">UM Accessories</h1>
+            <hr>
+          </div>
+          <div class="row">
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM Wooden</h5>
+                          <h5 class="card-title">Tumber</h5>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="col-md-3 mt-5">
+                  <div class="card custom-card-height" style="width: 100%;">
+                      <img src="/assets/images/esports.jpg" class="card-img-top" alt="..." height="210">
+                      <div class="card-body">
+                          <h5 class="card-title">UM CCE ESPORTS</h5>
+                          <h5 class="card-title">JERSEY</h5>
+                          <small class="card-text">Men</small>
+                          <div class="d-flex flex-row">
+                              <p class="card-text text-decoration-line-through text-danger">$150.00</p>
+                              <p class="card-text text-success">&nbsp;&nbsp;$120.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            </div>
         </div>
       </div>
     </section>
