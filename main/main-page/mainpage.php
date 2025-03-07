f3588112238a139022833181f42b1bca8b3f4ae2
yoshbatula <yoshbatula2@gmail.com>
2025-03-08 00:03:50 +0800

    argg

--- main/main-page/mainpage.php
+++ main/main-page/mainpage.php
@@ -11,6 +11,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
+    <!-- INCLUDE NAVIGATION BAR -->
     <?php include 'navigation.php'; ?>
     <div class="hero-section">
         <div class="overlay">
